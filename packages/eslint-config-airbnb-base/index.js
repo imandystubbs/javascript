0177d4f6290b50a9b8cf1571df8b479706bf8855
Andy Stubbs <55236473+imandystubbs@users.noreply.github.com>
2024-05-16 16:48:47 +0100

    convert to flat file format

--- packages/eslint-config-airbnb-base/index.js
+++ packages/eslint-config-airbnb-base/index.js
@@ -1,17 +1,27 @@
-module.exports = {
-  extends: [
-    './rules/best-practices',
-    './rules/errors',
-    './rules/node',
-    './rules/style',
-    './rules/variables',
-    './rules/es6',
-    './rules/imports',
-    './rules/strict',
-  ].map(require.resolve),
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  rules: {},
-};
+const bestPractices = require('./rules/best-practices');
+const errors = require('./rules/errors');
+const nodeRules = require('./rules/node');
+const style = require('./rules/style');
+const variables = require('./rules/variables');
+const es6 = require('./rules/es6');
+const imports = require('./rules/imports');
+const strict = require('./rules/strict');
+
+module.exports = [
+  // Include each rules configuration directly
+  bestPractices,
+  errors,
+  nodeRules,
+  style,
+  variables,
+  es6,
+  imports,
+  strict,
+  {
+    languageOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+    },
+    rules: {},
+  }
+];
